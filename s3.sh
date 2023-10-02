dad0156806bc24501703cbe7cbee91b1d423dade
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-10-02 17:54:48 +0530

    updated

--- s3.sh
+++ s3.sh
@@ -2,9 +2,9 @@
 sudo  yum install nginx -y
 cd /usr/share/nginx/html
 aws configure
-AWS Access Key ID:
-AWS Secret Access Key:AKIARO57XSWWXIQIRIG6
-Default region name :dI1UrRpTWhF7ff9AXT2JXATzpQC76W1qUDah1D8f
+AWS Access Key ID:AKIARO57XSWWXIQIRIG6
+AWS Secret Access Key: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
+Default region name :us-east-1
 aws s3 ls
 aws s3 ls s3://myproject54
 aws s3 cp s3://myproject54/index.html .
