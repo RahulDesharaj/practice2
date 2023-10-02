31b3b8cc0075fa7934a38d5eea6a90e39e041d62
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-10-02 17:58:53 +0530

    updated

--- s3.sh
+++ s3.sh
@@ -2,9 +2,9 @@
 sudo  yum install nginx -y
 cd /usr/share/nginx/html
 aws configure
-AKIARO57XSWWXIQIRIG6
-dI1UrRpTWhF7ff9AXT2JXATzpQC76W1qUDah1D8f
-us-east-1
+cp AKIARO57XSWWXIQIRIG6
+cp dI1UrRpTWhF7ff9AXT2JXATzpQC76W1qUDah1D8f
+cp us-east-1
 aws s3 ls
 aws s3 ls s3://myproject54
 aws s3 cp s3://myproject54/index.html .
