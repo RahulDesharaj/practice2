17c336a976caa2a07eab7cee2459efce5dbde694
RahulDesharaj <138120921+RahulDesharaj@users.noreply.github.com>
2023-10-03 22:25:57 +0530

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -2,8 +2,7 @@
 sudo  yum install nginx -y
 cd /usr/share/nginx/html
 aws configure
-cp AKIARO57XSWWXIQIRIG6
-cp dI1UrRpTWhF7ff9AXT2JXATzpQC76W1qUDah1D8f
+
 cp us-east-1
 aws s3 ls
 aws s3 ls s3://myproject54
